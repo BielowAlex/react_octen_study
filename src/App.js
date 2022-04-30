33acf21c7f7d21e7c9922c5c35d898a44ce9cd39
BielowAlex <bielow.alex@gmail.com>
2022-04-30 18:05:50 +0300

    LS6

--- src/App.js
+++ src/App.js
@@ -1,39 +1,64 @@
 import './App.css';
-import Users from "./сomponents/Users/Users";
+import {useReducer, useState} from "react";
+import {useForm} from "react-hook-form";
 
-import UserDetails from "./сomponents/UserDetails/UserDetails";
 
-import React, {useEffect, useState} from "react";
-import {userServices} from "./Services";
-import Post from "./сomponents/Post/Post";
-
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'catAdd':
+            return {...state, cats: [...state.cats, {name: action.payload, id: Date.now()}]}
+        case 'dogAdd':
+            return {...state, dogs: [...state.dogs, {name: action.payload, id: Date.now()}]}
+        case 'catDelete':
+            return {...state, cats: state.cats.filter(cat => cat.id !== action.payload)};
+        case 'dogDelete':
+            return {...state, dogs: state.dogs.filter(dog => dog.id !== action.payload)};
+    }
+}
 
 function App() {
-    const [user, setUser] = useState();
-    const [posts, setPosts] = useState([]);
-    let postsSec;
-    const getUser = (user) => {
-        setUser(user);
-        postsSec = document.querySelector('.posts_sec');
-        postsSec.classList.add('_hide');
+    const [state, dispatcher] = useReducer(reducer, {cats: [], dogs: []});
+
+    const {register, handleSubmit} = useForm();
+
+    const [cat, setCat] = useState(null);
+    const addCat = (data) => {
+        dispatcher({type: 'catAdd', payload: data.cat});
+    }
+    const addDog = (data) => {
+        dispatcher({type: 'dogAdd', payload: data.dog});
     }
 
-    const getUserPosts = (id) => {
-        userServices.getPostByUserID(id).then(({data}) => setPosts(data));
-        postsSec = document.querySelector('.posts_sec');
-        postsSec.classList.remove('_hide');
-    }
-
-
+    console.log(state);
     return (
         <div className="App">
-            <div className="wrap">
-                <Users getUser={getUser}/>
-                {user && <UserDetails user={user} getUserPosts={getUserPosts}/>}
+            <div className="forms">
+                <form className="cat" onSubmit={handleSubmit(addCat)}>
+                    <input type="text" placeholder='Cat' {...register('cat')}/>
+                    <button>Add</button>
+                </form>
+                <form className="dog" onSubmit={handleSubmit(addDog)}>
+                    <input type="text" placeholder='Dog' {...register('dog')}/>
+                    <button>Add</button>
+                </form>
             </div>
-            <div className="posts_sec _hide">
-                    <h2>Posts</h2>
-                {posts.map(post => <Post key={post.id} post={post}/>)}
+            <div className="out">
+                <ul className="cat_out">
+                    {state.cats && state.cats.map(cat =>
+                        <li key={cat.id}>
+                            {cat.name}
+                            <button onClick={()=>dispatcher({type:'catDelete',payload:cat.id})}>delete</button>
+                        </li>
+                    )}
+                </ul>
+                <ul className="dog_out">
+                    {state.dogs && state.dogs.map(dog =>
+                        <li key={dog.id}>
+                            {dog.name}
+                            <button onClick={()=>dispatcher({type:'dogDelete',payload:dog.id})}>delete</button>
+                        </li>
+                    )}
+                </ul>
             </div>
         </div>
     );
